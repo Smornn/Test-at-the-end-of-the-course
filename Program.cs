29073a23040e3a85ac4fa21d5d6a5ceee0ffe96f
Alexey Parshukov <a79042302508@gmail.com>
2024-04-14 11:59:14 +0300

    create code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] start_mas = { "1234", "1567", "-2", "computer science" };
+string[] second_mas = new string[start_mas.Length];
+
+void FindSecondMas (string[] start_mas, string[] second_mas)
+{
+    int temp = 0;
+    for (int i = 0; i < start_mas.Length; i++)
+    {
+        if (start_mas[i].Length <= 3)
+        {
+            second_mas[temp] = start_mas[i];
+            temp++;
+        }
+    }
+}
+
+void PrintArray(string[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.Write($"{mas[i]} ");
+    }
+    Console.WriteLine();
+}
+
+FindSecondMas(start_mas, second_mas);
+PrintArray(second_mas);
